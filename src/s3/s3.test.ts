f53cd3aea4b0f7529c73036be6a1ee0e8e44265f
grugbit <86127892+aidlran@users.noreply.github.com>
2024-11-27 21:58:53 +0000

    test(s3): use `runIf` instead

--- src/s3/s3.test.ts
+++ src/s3/s3.test.ts
@@ -1,27 +1,25 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { test } from 'vitest';
+import { describe } from 'vitest';
 import { testRPCStrategyForContent } from '../../testing/rpc-strategy.js';
 import { s3 } from './s3.js';
 
-const Bucket = process.env.TEST_S3_BUCKET;
-const ENDPOINT = process.env.TEST_S3_ENDPOINT;
-const KEY = process.env.TEST_S3_KEY;
-const KEY_ID = process.env.TEST_S3_KEY_ID;
-const REGION = process.env.TEST_S3_REGION;
+const {
+  TEST_S3_BUCKET: BUCKET,
+  TEST_S3_ENDPOINT: ENDPOINT,
+  TEST_S3_KEY: KEY,
+  TEST_S3_KEY_ID: KEY_ID,
+  TEST_S3_REGION: REGION,
+} = process.env;
 
-if (!Bucket || !ENDPOINT || !KEY || !KEY_ID || !REGION) {
-  // eslint-disable-next-line no-console
-  console.warn('Missing TEST_S3_* from environment. Skipping S3Driver tests.');
-  test.todo('S3 [skipped]');
-} else {
+describe.runIf(BUCKET && ENDPOINT && KEY && KEY_ID && REGION)('S3 client', () => {
   const Client = new S3Client({
     credentials: {
-      accessKeyId: KEY_ID,
-      secretAccessKey: KEY,
+      accessKeyId: KEY_ID!,
+      secretAccessKey: KEY!,
     },
     region: REGION,
     endpoint: ENDPOINT,
   });
 
-  testRPCStrategyForContent('S3', s3({ Bucket, Client }));
-}
+  testRPCStrategyForContent('S3', s3({ Bucket: BUCKET!, Client }));
+});
