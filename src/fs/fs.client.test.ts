9ff0949d3a8c80731bc82fa678faa6c7f668ffcb
aidlran <86127892+aidlran@users.noreply.github.com>
2026-01-30 18:36:32 +0000

    test(fs): fix intermittent test failure

--- src/fs/fs.client.test.ts
+++ src/fs/fs.client.test.ts
@@ -1,19 +1,16 @@
-import { mkdir, rm, writeFile } from 'fs/promises';
-import { join, resolve } from 'path';
+import { mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'fs';
+import { join } from 'path';
 import { afterAll, describe, expect, test } from 'vitest';
 import { testRPCStrategyForContent } from '../../testing/rpc-strategy.js';
 import fs from './fs.client.js';
 
 describe('Filesystem client', async () => {
-  const dir = resolve('.', 'tmp');
-  const enotdirTestFile = join(dir, 'ENOTDIR_TEST_FILE');
+  const dir = mkdtempSync('astrobase-test-');
   const eaccesTestDir = join(dir, 'EACCES_TEST_DIR');
+  const enotdirTestFile = join(dir, 'ENOTDIR_TEST_FILE');
 
-  await Promise.all([
-    mkdir(dir, { recursive: true }),
-    writeFile(enotdirTestFile, ''),
-    mkdir(eaccesTestDir, { mode: 0o555 }),
-  ]);
+  mkdirSync(eaccesTestDir, { mode: 0o555 });
+  writeFileSync(enotdirTestFile, '');
 
   testRPCStrategyForContent('Filesystem', await fs({ dir }));
 
@@ -23,5 +20,5 @@ describe('Filesystem client', async () => {
   test('Correctly throws EACCES', () =>
     expect(fs({ dir: eaccesTestDir })).rejects.toThrow('EACCES'));
 
-  afterAll(() => rm(dir, { force: true, recursive: true }));
+  afterAll(() => rmSync(dir, { force: true, recursive: true }));
 });
